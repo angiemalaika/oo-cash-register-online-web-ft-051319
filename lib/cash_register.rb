cc52faa029c6f6eaad7282a2f1a22a1e929661d7
Angela Mumba <malaika.mumba@gmail.com>
2019-05-26 05:08:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
-
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0 
+  end 
+  
+  def add_item(title,price,quatity = 0)
+    @total += price
+    
+  end 
+end 
